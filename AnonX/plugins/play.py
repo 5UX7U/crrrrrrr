7ac029d5c3c888886248de8b7bfaabd89cece769
tomyboot <129333584+tomyboot@users.noreply.github.com>
2023-03-30 02:26:46 +0200

    Add files via upload

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -6,12 +6,12 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
+
 import random
 import string
 from ast import ExceptHandler
-
 from pyrogram import filters
-from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto,
+from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
                             Message)
 from pytgcalls.exceptions import NoActiveGroupCall
 
@@ -20,22 +20,20 @@ from config import BANNED_USERS, lyrical
 from strings import get_command
 from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from AnonX.core.call import Anon
+from AnonX.core.call import Yukki
 from AnonX.utils import seconds_to_min, time_to_seconds
 from AnonX.utils.channelplay import get_channeplayCB
 from AnonX.utils.database import is_video_allowed
 from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.decorators.play import PlayWrapper
+from AnonX.utils.decorators.play import PlayWrapper, PlayWrapperCHH
 from AnonX.utils.formatters import formats
 from AnonX.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from AnonX.utils.database import is_served_user
 from AnonX.utils.inline.playlist import botplaylist_markup
 from AnonX.utils.logger import play_logs
 from AnonX.utils.stream.stream import stream
 from strings.filters import command
-
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 PLAY_COMMAND_chn = get_command("PLAY_COMMAND_chh")
